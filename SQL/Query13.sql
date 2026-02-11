86bcb5cd61b6321ff30654d90ff03a4687313cc3
Jayanth C <jayanthjayanthc96@gmail.com>
2026-02-11 18:50:39 +0530

    Daily Progress

---
+++ SQL/Query13.sql
@@ -0,0 +1,8 @@
+/*
+Enter your query here.
+Query the list of CITY names from STATION that do not start with vowels 
+and do not end with vowels. Your result cannot contain duplicates.
+*/
+
+SELECT Distinct CITY from STATION where RIGHT(CITY,1) NOT IN ('a','e','i','o','u') AND
+LEFT(CITY,1) NOT IN ('a','e','i','o','u');

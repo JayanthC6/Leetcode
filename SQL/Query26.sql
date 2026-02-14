48c1b8d4868e54a02a6d7d7556bbd4e99d6c00d7
Jayanth C <jayanthjayanthc96@gmail.com>
2026-02-14 21:42:39 +0530

    Daily progress

---
+++ SQL/Query26.sql
@@ -0,0 +1,6 @@
+/*  Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+SELECT sum(city.population) from CITY join country on city.countrycode
+= country.code where country.CONTINENT = 'Asia';
---
+++ SQL/Query27.sql
@@ -0,0 +1,6 @@
+/* Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns. */
+
+SELECT city.name from city join country on city.countrycode = country.code 
+where country.continent='Africa';

ac9c6a8a233543f9454580b43005618d2f94b1da
Jayanth C <jayanthjayanthc96@gmail.com>
2026-02-17 19:50:52 +0530

    updated code

--- Python/Reverse_bits.py
+++ Python/Reverse_bits.py
@@ -1,8 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        result=0
+        res=0
         for _ in range(32):
-            bit = n&1
-            result=(result<<1) | bit
-            n=n>>1
-        return result    
+            res=(res<<1)|(n&1)
+            n>>=1
+        return res        

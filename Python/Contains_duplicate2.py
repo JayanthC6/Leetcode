f1f10ea506ec07d3138f3b00abe93be126c3829a
Jayanth C <jayanthjayanthc96@gmail.com>
2026-02-21 20:19:04 +0530

    Daily Progress

--- Python/Contains_duplicate2.py
+++ Python/Contains_duplicate2.py
@@ -1,12 +1,21 @@
-class Solution:
+'''class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]==nums[j] and abs(i - j) <= k:
                     return True
-        return False            
+        return False'''           
     
-    '''Example 1:
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        last = {}
+        for i, n in enumerate(nums):
+            if n in last and i - last[n] <= k:
+                return True
+            last[n] = i
+        return False
+
+'''Example 1:
 
 Input: nums = [1,2,3,1], k = 3
 Output: true

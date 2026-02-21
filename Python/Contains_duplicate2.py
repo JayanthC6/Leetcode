5ec28a21de4a53881c23d0dbfb640c5f4db7fe33
Jayanth C <jayanthjayanthc96@gmail.com>
2026-02-21 20:08:49 +0530

    Daily Progress

---
+++ Python/Contains_duplicate2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j] and abs(i - j) <= k:
+                    return True
+        return False            
+    
+    '''Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false'''

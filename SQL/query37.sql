05a2c991b0b67a2f4ab1ed009950bdfc8850720f
Jayanth C <jayanthjayanthc96@gmail.com>
2026-02-25 20:45:08 +0530

    Daily Progress

---
+++ SQL/query37.sql
@@ -0,0 +1,17 @@
+/*
+Enter your query here.
+ Write a query to print the id, age, coins_needed, and power of the wands that Ron's interested in, 
+ sorted in order of descending power. 
+ If more than one wand has same power, sort the result in order of descending age.
+*/
+SELECT w.id, p.age, w.coins_needed, w.power
+FROM Wands w
+JOIN Wands_Property p ON w.code = p.code
+WHERE p.is_evil = 0
+AND w.coins_needed = (
+    SELECT MIN(w2.coins_needed)
+    FROM Wands w2
+    JOIN Wands_Property p2 ON w2.code = p2.code
+    WHERE p2.age = p.age AND w2.power = w.power
+)
+ORDER BY w.power DESC, p.age DESC;

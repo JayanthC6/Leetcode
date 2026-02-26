cba80cf362a175af97351e2869386f9064bdc824
Jayanth C <jayanthjayanthc96@gmail.com>
2026-02-26 10:26:40 +0530

    Daily Progress

---
+++ Python/Reverse_interger.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        is_neg=x<0
+        x=abs(x)
+        res=0
+        while x:
+            digit=x%10
+            x//=10
+            res=res*10+digit
+            if res>2**31-1:
+                return 0
+        return res  *-1 if is_neg else res   
+
+'''Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21'''

8474aae89c78653b8ff8cc2b62cb81335046c6a2
Jayanth C <jayanthjayanthc96@gmail.com>
2026-03-05 19:56:52 +0530

    Daily progress

---
+++ Python/Rotate_array.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rotate(self,nums:list[int],k:int)->None:
+        n=len(nums)
+        k=k%n
+        def reverse(start:int,end:int):
+            while start<end:
+                nums[start],nums[end]=nums[end],nums[start]
+                start+=1
+                end-=1
+        reverse(0,n-1)
+        reverse(0,k-1)
+        reverse(k,n-1)
+#Time Complexity: O(N) where N is the length of the input array nums. We reverse the entire array and then reverse two subarrays, each of which takes O(N) time in total.
+'''Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]'''         

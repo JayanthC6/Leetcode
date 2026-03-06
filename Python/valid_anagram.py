737edd481fbb8d716b76a282dff2c21b423cf2c4
Jayanth C <jayanthjayanthc96@gmail.com>
2026-03-06 20:57:53 +0530

    Daily progress

--- Python/valid_anagram.py
+++ Python/valid_anagram.py
@@ -1,3 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s)==sorted(t)
+ '''Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false'''   

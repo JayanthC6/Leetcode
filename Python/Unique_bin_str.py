11f6151d204c6e369632055fda3f6c92925ca302
JayanthC6 <jayanthjayanthc96@gmail.com>
2026-04-25 22:34:38 +0530

    Random problem

--- Python/Unique_bin_str.py
+++ Python/Unique_bin_str.py
@@ -9,13 +9,10 @@ class Solution:
                 ans.append('0')
         return "".join(ans)           
 
+
 #time complexity: O(n)
 '''Given an array of strings nums containing n unique binary strings each of length n, return a binary string of length n that does not appear in nums. If there are multiple answers, you may return any of them.
-
- 
-
 Example 1:
-
 Input: nums = ["01","10"]
 Output: "11"
 Explanation: "11" does not appear in nums. "00" would also be correct.
